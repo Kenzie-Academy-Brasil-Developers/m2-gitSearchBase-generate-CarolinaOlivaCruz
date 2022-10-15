61d15b97fc115313690adce93472aa5470881802
Ana Carolina Oliveira <carol.oliva.cruz@gmail.com>
2022-10-15 11:35:44 -0300

    function

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -2,15 +2,20 @@
 function requisicao() {
   fetch("https://api.github.com/users/carolinaolivacruz")
     .then((response) => response.json()).then((response) => {
-      // console.log(response)
+      console.log(response)
       pageUser(response)
     });
 }
 
 
-
-
 function pageUser(user) {
+  const
+    {
+      avatar_url,
+      name,
+      company,
+      repos_url
+    } = user
 
   const body = document.getElementById('bodyUser')
 
@@ -19,13 +24,13 @@ function pageUser(user) {
   const divheader = document.createElement('div')
 
   const imgUser = document.createElement('img')
-  imgUser.src = user.avatar_url
+  imgUser.src = avatar_url
 
   const divUser = document.createElement('div')
   const h3User = document.createElement('h3')
-  h3User.innerText = user.name
+  h3User.innerText = name
   const pUser = document.createElement('p')
-  pUser.innerText = user.company
+  pUser.innerText = company
 
   const divButton = document.createElement('div')
   const buttonEmail = document.createElement('button')
@@ -36,29 +41,37 @@ function pageUser(user) {
   const main = document.createElement('main')
 
   const ulRepository = document.createElement('ul')
-  
-  const repository = user.repos_url
-  console.log(repository)
 
-  repository.forEach(element => {
-    
-    const repositoryId = element.id
-    console.log(repositoryId)
-
-    const liRepository = document.createElement('li')
-
-    const h4Repository = document.createElement('h4')
-    h4Repository.innerText = ''
-    const pRepository = document.createElement('p')
-    const divRepository = document.createElement('div')
-    const buttonRepository = document.createElement('h4')
-    const h4Demo = document.createElement('h4')
+  const repository = repos_url
 
 
-    divRepository.append(buttonRepository, h4Demo)
-    liRepository.append(h4Repository, pRepository, divRepository)
-    ulRepository.appendChild(liRepository)
-  });
+  function requisicaoRepository() {
+    fetch(repository)
+      .then((response) => response.json()).then((response) => {
+        response.forEach(element => {
+
+          const repositoryId = element.id
+          console.log(repositoryId)
+
+          const liRepository = document.createElement('li')
+
+          const h4Repository = document.createElement('h4')
+          h4Repository.innerText = ''
+          const pRepository = document.createElement('p')
+          const divRepository = document.createElement('div')
+          const buttonRepository = document.createElement('h4')
+          const h4Demo = document.createElement('h4')
+
+          divRepository.append(buttonRepository, h4Demo)
+          liRepository.append(h4Repository, pRepository, divRepository)
+          ulRepository.appendChild(liRepository)
+        });
+
+
+      });
+  }
+  requisicaoRepository()
+
 
 
   divUser.append(h3User, pUser)
